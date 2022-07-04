5795bc8e50fda22a8d2c697a059de0be4a14e011
duckbillLvr <86183856+duckbillLvr@users.noreply.github.com>
2022-07-04 23:45:16 +0900

    Instagram clone coding

---
+++ Instagram/src/main/java/zeno/instagram/config/secret/Secret.java
@@ -0,0 +1,8 @@
+package zeno.instagram.config.secret;
+
+// TODO: 해당 KEY 값들을 꼭 바꿔서 사용해주세요!
+// TODO: .gitignore에 추가하는거 앚지 마세요!
+public class Secret {
+    // 임의의 JWT 키 발급
+    public static String JWT_SECRET_KEY = "UwKYibQQgkW7g-*k.ap9kje-wxBHb9wdXoBT4vnt4P3sJWt-Nu";
+}
